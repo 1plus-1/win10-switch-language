470ba13a6a54f6c1c12c975e426ff1f49cc6913b
U-GLOBAL\E517845 <E517845@CH3ULT92S0Q72.global.ds.honeywell.com>
2018-03-22 15:32:40 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# win10-switch-language
-A simple python program to switch language on Win10
+win10-switch-language
+========
+
+A simple python program to switch language on Win10. For whom used to use Ctrl+Space to switch between input languages, this tool could be useful on Win10.
+
+
+## Usage
+
+- Install python on Win10. (Tested with python 3.6 only.)
+- Install keyboard package
+	pip install keyboard
+- Run "pythonw SwitchLanguage.py" manually or add it to batch file inside startup folder to enable it in system startup
---
+++ SwitchLanguage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from keyboard import on_press, on_release, send, wait
+from win32gui import GetWindowText, GetForegroundWindow
+from datetime import datetime
+
+def KeyPressed(event):
+	global bCtrlDown, bSpaceDown, bSwitchLan
+	if event.name == 'space':
+		bSpaceDown = True
+	if(event.name == 'left ctrl' or event.name == 'right ctrl'):
+		bCtrlDown = True;
+	if event.name == 'space' and bCtrlDown:	#space press during ctrl down
+		bSwitchLan = True
+
+def KeyReleased(event):
+	global bCtrlDown, bSpaceDown, bSwitchLan
+	if event.name == 'space':
+		bSpaceDown = False
+		if bSwitchLan and not bCtrlDown:
+			bSwitchLan = False
+			send('left windows+space')
+	elif(event.name == 'left ctrl' or event.name == 'right ctrl'):
+		bCtrlDown = False
+		if bSwitchLan and not bSpaceDown:
+			bSwitchLan = False
+			send('left windows+space')
+
+bCtrlDown = bSpaceDown = bSwitchLan = False
+on_press(KeyPressed)
+on_release(KeyReleased)
+wait()
